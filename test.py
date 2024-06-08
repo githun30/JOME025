23228f13199d1fe4b2a7a1cf5488a16c2320747b
githun30 <165976159+githun30@users.noreply.github.com>
2024-06-08 20:32:13 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 import matplotlib.pyplot as plt
 from matplotlib import font_manager
 
-# 폰트 파일의 정확한 경로 지정
-font_path = 'https://github.com/githun30/JOME025/blob/main/NanumGothic.ttf'
+# 폰트 파일의 정확한 로컬 경로 지정
+font_path = 'C:/Users/KMGHO/Downloads/NanumGothic.ttf'
 font_prop = font_manager.FontProperties(fname=font_path)
 
 # 로드된 폰트 이름 출력
-print("Loaded font name:", font_prop.get_name())
+try:
+    print("Loaded font name:", font_prop.get_name())
+except Exception as e:
+    print("Error loading font:", e)
