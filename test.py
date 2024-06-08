7b6f2fc783a42bc2718433f6f265996a6fc8fc5e
githun30 <165976159+githun30@users.noreply.github.com>
2024-06-08 20:37:49 +0900

    Update test.py

--- test.py
+++ test.py
@@ -5,8 +5,10 @@ from matplotlib import font_manager
 font_path = 'C:/Users/KMGHO/Downloads/NanumGothic.ttf'
 font_prop = font_manager.FontProperties(fname=font_path)
 
-# 로드된 폰트 이름 출력
-try:
-    print("Loaded font name:", font_prop.get_name())
-except Exception as e:
-    print("Error loading font:", e)
+# matplotlib의 기본 폰트 설정
+plt.rcParams['font.family'] = font_prop.get_name()
+
+# 테스트 그래프 생성
+plt.figure()
+plt.title('테스트 그래프', fontproperties=font_prop)
+plt.plot([1, 2, 3], [4, 5, 6])
